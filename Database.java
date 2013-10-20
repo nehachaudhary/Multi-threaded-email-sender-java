8eec813781c37fe4fd863f896d4fc6ee0804b251
Shivam Sood <shivamsood89@gmail.com>
2013-10-20 21:20:20 +0530

    Update Database.java
    Variables are taken at the starting of the class and are declared static final. startConnection is renamed to getConnection.

--- Database.java
+++ Database.java
@@ -9,29 +9,43 @@ import com.mysql.jdbc.PreparedStatement;
 /* This class handles all Database related stuff */
 public class Database {
 
+	private static final String DATABASE_STRING = "jdbc:mysql://localhost:3307/emails";
+	private static final String USERNAME = "sqluser";
+	private static final String PASSWORD = "sqluserpw";
+
+	// Can be changed accordingly.
+	private static final int RECORDS_LIMIT = 100;
+
+	private static final String SELECT_QUERY = "select * from EmailQueue where sent='false' LIMIT ";
+	private static final String UPDATE_QUERY = "Update EmailQueue SET sent=true where id=?";
+
+	// have renamed the function to getConnection. StartConnection doesn't make
+	// any sense to return the connection.
 	/* Function to start connection with database */
-	public static Connection startConnection() {
+	public static Connection getConnection() {
 		Connection connection = null; // initialize a variable connection of
 										// type Connection to NULL
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); // JDBC driver for MySql
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			// Instead of printing stack Trace, throw the exception. Program
+			// should stop here.
 			e.printStackTrace();
 		}
 		try {
 
 			/*
 			 * here we pass the parameters to connect to Database
-			 * emails","sqluser","sqluserpw"    ->  "emails" is the database name
-			 *                                  ->  "sqluser" is the username for the Mysql Database 
-			 *                                  ->  "sqluserpw" is the password for the MySql Database
+			 * emails","sqluser","sqluserpw"    ->  "emails" is the database
+			 * name -> "sqluser" is the username for the Mysql Database ->
+			 * "sqluserpw" is the password for the MySql Database
 			 */
-			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3307/emails", "sqluser",
-					"sqluserpw");
+			connection = DriverManager.getConnection(DATABASE_STRING, USERNAME,
+					PASSWORD);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// What if some exception has come & connection is initiazlied to
+			// null?
+			// Just catching the exception will help?
 			e.printStackTrace();
 		}
 		return connection; // return connection variable.
@@ -48,7 +62,7 @@ public class Database {
 	}
 
 	/*
-	 * This function fetches specified number of records from the table we
+	 * This function fetches specified number of records from the table as we
 	 * cannot fetch all the records in one go since Database can have millions
 	 * of records so we can limit our SELECT query to fetch only limited
 	 * records.
@@ -68,22 +82,25 @@ public class Database {
 			 * Select all columns from table EmailQueue where sent is false
 			 * Limited to 100 records only
 			 */
-			rs = stmt.executeQuery("select * from EmailQueue where sent='false' LIMIT 100");
+			rs = stmt.executeQuery(SELECT_QUERY + RECORDS_LIMIT);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return rs; // Return the resultset rs.
 	}
-    
-	
-	/* This function updates the status of all the emails that are successfully sent from sent=false to sent=true
-	 * so that no email is sent twice by any other thread. */
+
+	/*
+	 * This function updates the status of all the emails that are successfully
+	 * sent from sent=false to sent=true so that no email is sent twice by any
+	 * other thread.
+	 */
 	public static void updateDataInTable(Connection connection, int id) {
 		java.sql.PreparedStatement stmt = null;
 		try {
-			String sqlQuery = "Update EmailQueue SET sent=true where id=?";  // Update table EmailQueue and set sent=true for every id that the function gets as argument.
-			stmt = connection.prepareStatement(sqlQuery);
+			// Update table EmailQueue and set sent=true for every id that the
+			// function gets as argument.
+			stmt = connection.prepareStatement(UPDATE_QUERY);
 			stmt.setInt(1, id);
 			stmt.executeUpdate();
 		} catch (SQLException e) {
